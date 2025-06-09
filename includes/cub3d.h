435e0e8c183820b681ef87976e4d333df71cccf1
Raain <ryanbwgt@gmail.com>
2025-06-09 13:26:23 +0200

    added mlx lib and fixed some file structure

---
+++ includes/cub3d.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   cub3d.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rmakoni <rmakoni@student.42heilbronn.de    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/09 13:20:42 by rmakoni           #+#    #+#             */
+/*   Updated: 2025/06/09 13:22:58 by rmakoni          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CUB3D_H
+# define CUB3D_H
+
+# include <math.h>
+# include "../libraries/libft/libft.h"
+# include "../libraries/MLX42/include/MLX42/MLX42.h"
+
+
+#endif
