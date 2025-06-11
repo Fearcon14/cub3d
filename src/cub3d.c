003905a23f31811e087ef740a650c4c7614a01fa
Kevin Sinn <kevinsinn01@gmail.com>
2025-06-11 13:41:06 +0200

    should now correctly check for file extension

--- src/cub3d.c
+++ src/cub3d.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   cub3d.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: rmakoni <rmakoni@student.42heilbronn.de    +#+  +:+       +#+        */
+/*   By: ksinn <ksinn@student.42heilbronn.de>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/09 13:14:04 by rmakoni           #+#    #+#             */
-/*   Updated: 2025/06/09 14:28:46 by rmakoni          ###   ########.fr       */
+/*   Updated: 2025/06/11 13:39:37 by ksinn            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,19 +16,23 @@
  * @brief Print error message and exit the program
  * @param msg The error message to display
  */
-void    error_exit(char *msg)
+void	error_exit(char *msg)
 {
-    ft_printf("Error\n%s", msg);
-    exit(1);
+	ft_printf("Error\n%s", msg);
+	exit(1);
 }
 
-int main(int argc, char **argv)
+// DONE: Map extension check should check ending not beginning of string
+int	main(int argc, char **argv)
 {
-    if (argc != 2)
-        error_exit("Invalid number of arguments");
-    if (ft_strncmp(argv[1], ".cub", 4) != 0)
-        error_exit("Invalid file extension");
-    if (validate_map(argv[1]) == 0)
-        error_exit("Invalid map");
-    return 1;
+	if (argc != 2)
+		error_exit("Invalid number of arguments");
+	if (ft_strlen(argv[1]) < 4 || ft_strncmp(&argv[1][ft_strlen(argv[1]) - 4],
+			".cub", 4) != 0)
+	{
+		error_exit("Invalid file extension");
+	}
+	if (validate_map(argv[1]) == 0)
+		error_exit("Invalid map");
+	return (1);
 }
