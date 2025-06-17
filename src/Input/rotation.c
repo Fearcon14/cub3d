fb8f27c41fb3339c82ae36ad8ff6873b3efb1578
Kevin Sinn <kevinsinn01@gmail.com>
2025-06-17 14:26:50 +0200

    added TODO to rotate

--- src/Input/rotation.c
+++ src/Input/rotation.c
@@ -6,7 +6,7 @@
 /*   By: ksinn <ksinn@student.42heilbronn.de>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/17 13:16:44 by ksinn             #+#    #+#             */
-/*   Updated: 2025/06/17 14:06:51 by ksinn            ###   ########.fr       */
+/*   Updated: 2025/06/17 14:26:33 by ksinn            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,10 +21,12 @@ static void	rotate_right(t_game *game)
 	old_plane_x = game->player.plane.x;
 	game->player.dir.x = game->player.dir.x * cos(ROTATION_SPEED)
 		- game->player.dir.y * sin(ROTATION_SPEED);
-	game->player.dir.y = old_dir_x * sin(ROTATION_SPEED) + game->player.dir.y * cos(-ROTATION_SPEED);
+	game->player.dir.y = old_dir_x * sin(ROTATION_SPEED) + game->player.dir.y
+		* cos(-ROTATION_SPEED);
 	game->player.plane.x = game->player.plane.x * cos(ROTATION_SPEED)
 		- game->player.plane.y * sin(ROTATION_SPEED);
-	game->player.plane.y = old_plane_x * sin(ROTATION_SPEED) + game->player.plane.y * cos(ROTATION_SPEED);
+	game->player.plane.y = old_plane_x * sin(ROTATION_SPEED)
+		+ game->player.plane.y * cos(ROTATION_SPEED);
 }
 
 static void	rotate_left(t_game *game)
@@ -36,17 +38,21 @@ static void	rotate_left(t_game *game)
 	old_plane_x = game->player.plane.x;
 	game->player.dir.x = game->player.dir.x * cos(-ROTATION_SPEED)
 		- game->player.dir.y * sin(-ROTATION_SPEED);
-	game->player.dir.y = old_dir_x * sin(-ROTATION_SPEED) + game->player.dir.y * cos(-ROTATION_SPEED);
+	game->player.dir.y = old_dir_x * sin(-ROTATION_SPEED) + game->player.dir.y
+		* cos(-ROTATION_SPEED);
 	game->player.plane.x = game->player.plane.x * cos(-ROTATION_SPEED)
 		- game->player.plane.y * sin(-ROTATION_SPEED);
-	game->player.plane.y = old_plane_x * sin(-ROTATION_SPEED) + game->player.plane.y * cos(-ROTATION_SPEED);
+	game->player.plane.y = old_plane_x * sin(-ROTATION_SPEED)
+		+ game->player.plane.y * cos(-ROTATION_SPEED);
 }
 
+// TODO: plane rotation
 void	rotate_player(t_game *game, mlx_key_data_t keydata)
 {
 	if (keydata.key == MLX_KEY_LEFT)
 		rotate_left(game);
 	else if (keydata.key == MLX_KEY_RIGHT)
 		rotate_right(game);
-	printf("Player direction: %f, %f\n", game->player.dir.x, game->player.dir.y);
+	printf("Player direction: %f, %f\n", game->player.dir.x,
+		game->player.dir.y);
 }
